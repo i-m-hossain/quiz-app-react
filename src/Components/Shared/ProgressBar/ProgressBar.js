ecb12ce8d555c656109c67798c5384ecffe44bd1
mdimran1409036 <mdimran1409036@gmail.com>
2022-01-30 12:45:56 +0600

    tooltip added on progressbar

--- src/Components/Shared/ProgressBar/ProgressBar.js
+++ src/Components/Shared/ProgressBar/ProgressBar.js
@@ -1,21 +1,36 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useRef, useState } from "react";
 import Button from "../Button/Button";
 import classes from "./ProgressBar.module.css";
 
 const ProgressBar = ({ prev, next, progress, submit }) => {
-    console.log(progress);
+    const [tooltip, setTooltip] = useState(false);
+    const tooltipRef = useRef();
+    const toggleTooltip = () => {
+        if (tooltip) {
+            setTooltip(false);
+            tooltipRef.current.style.display = "none";
+        } else {
+            setTooltip(true);
+            tooltipRef.current.style.display = "block";
+            tooltipRef.current.style.left = `calc(${progress}% - 65px)`;
+        }
+    };
+
     return (
         <div className={classes.progressBar}>
             <div className={classes.backButton} onClick={prev}>
                 <span className="material-icons-outlined">arrow_back</span>
             </div>
             <div className={classes.rangeArea}>
-                <div className={classes.tooltip}>{progress}% Complete!</div>
+                <div className={classes.tooltip} ref={tooltipRef}>
+                    {progress}% Complete!
+                </div>
                 <div className={classes.rangeBody}>
                     <div
                         className={classes.progress}
                         style={{ width: `${progress}%` }}
+                        onMouseOut={toggleTooltip}
+                        onMouseOver={toggleTooltip}
                     ></div>
                 </div>
             </div>
